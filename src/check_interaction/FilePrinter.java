f64c7456de6207df5c36c292837d66c830fbb900
Bill Schofield <bill@billschofield.net>
2013-08-17 10:57:56 -0500

    First Passing Interaction Test

--- src/check_interaction/FilePrinter.java
+++ src/check_interaction/FilePrinter.java
@@ -3,9 +3,15 @@ package check_interaction;
 import java.io.PrintStream;
 
 public class FilePrinter {
+    private final FileLineReader fileLineReader;
+    private final PrintStream printStream;
+
     public FilePrinter(FileLineReader fileLineReader, PrintStream printStream) {
+        this.fileLineReader = fileLineReader;
+        this.printStream = printStream;
     }
 
     public void print() {
+        printStream.println(fileLineReader.lines().get(0));
     }
 }
